51161e771ef02cee296a802d5803de7d96d7e4fa
Николай <Filippovnikolya@yandex.ru>
2022-07-30 19:33:47 +0300

    first commit

---
+++ src/main/java/StatService.java
@@ -0,0 +1,26 @@
+public class StatService {
+
+    public long sum(long[] sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / 12;
+    }
+
+    public int countBelowAvg(long[] sales) {
+        int count = 0;
+        long avg = average(sales);
+        for (long sale : sales) {
+            if (sale < avg) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
